95f6f18dc3608517ebc810341ae87a66649ff5af
Manjusri Arumugam <msri10792@gmail.com>
2024-10-19 03:25:37 +0530

    insertion.py

--- insertion.py
+++ insertion.py
@@ -3,8 +3,10 @@ def insertion_sort(arr):
         key = arr[i]
         j = i - 1
 
-        # Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+        # Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
         while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
+
+    return arr  # Return the sorted array
