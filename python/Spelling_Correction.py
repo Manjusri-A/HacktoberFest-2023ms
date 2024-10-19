574a275f5dc22cffe9262b9a770a38135175d5da
Manjusri Arumugam <msri10792@gmail.com>
2024-10-19 07:04:12 +0530

    Spelling_Correction.py

--- python/Spelling_Correction.py
+++ python/Spelling_Correction.py
@@ -1,16 +1,18 @@
 from textblob import TextBlob
 
 def Convert(string):
+    # Convert the string to a list of words
     li = list(string.split())
     return li
-str1 = input("Enter your word :")
-words=Convert(str1)
+
+str1 = input("Enter your words: ")  # Changed "word" to "words" for clarity
+words = Convert(str1)
 corrected_words = []
 
+# Correct spelling for each word
 for i in words:
-    corrected_words.append(TextBlob(i))
-print("wrong words : ", words)
-print("corrected words are : ")
-for i in corrected_words:
-    print(i.correct(), end=" ")
+    corrected_words.append(TextBlob(i).correct())  # Correct spelling directly here
 
+print("Wrong words: ", words)
+print("Corrected words are: ")
+print(" ".join(str(word) for word in corrected_words))  # Print corrected words as a string
